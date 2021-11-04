7b36d29b27626145a820cf0b506d2cf1ad537c8a
Illyaz Arya <71999096+McIllyaz@users.noreply.github.com>
2021-11-04 11:25:03 +0800

    Add files via upload

---
+++ hi_kamu.py
@@ -0,0 +1,40 @@
+print("hello 'your crush name' ")
+name=input('put your name here please my dear')
+realname='(her/his name)'
+
+if name==realname:
+    print('hello','(her/his name)', '!❤️')
+else:
+    print('Dont lie to me, your name is (her/his name) right?')
+
+print('Introducing i am a program who made by (your name)')
+print(' ')
+print(' ')
+bfname=str(input('Who is your gf/bf?'))
+bfrealname="(her/his name)"
+
+if bfname==bfrealname:
+    print('I know right ✌')
+else:
+    print("Why you lyinggggg 😥😭")
+
+print(' ')
+print(' ')
+
+sakit=input('do you still love me? (yes/no) :')
+
+while sakit!='yes':
+    sakit=input('you lying :( do you still love me? (yes/no) :')
+print(' ')
+print(' ')
+banyak=int(input('how many times you want me to say i hate you? '))
+while banyak<=0:
+    banyak=int(input('oh come on, how many times you want me to say i hate you? '))
+
+for i in range (banyak):
+    print('LOVE YOU (HER/HIS NAME)! <3')
+
+
+
+while True:
+    pass
